e3b715f8f303074634098c0afdb8032087a9809a
Abhinav appuriabhi <appuriabhi@gmail.com>
2024-06-14 18:08:16 +0530

    cookie_setup_fpid

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+from datetime import datetime, timedelta
+import uuid
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    cookie_name = 'FPID'
+    cookie_value = request.cookies.get(cookie_name)
+
+    if not cookie_value:
+        cookie_value = str(uuid.uuid4())
+
+    expires = datetime.now()
+    expires = expires + timedelta(days=30*13)
+    response = make_response()
+    response.set_cookie(cookie_name, cookie_value, expires=expires, secure=True, httponly=True, samesite='Lax', domain='fpid-flaskapp.onrender.com')
 
 @app.route('/')
 def home():
